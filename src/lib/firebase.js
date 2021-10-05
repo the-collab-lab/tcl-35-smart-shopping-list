acd74b892fc758499c87194d9ea607b4fc52512a
Retno Ayu Adiati <retno_ayu@hotmail.com>
2021-10-05 10:32:06 +0200

    Update firebase.js to v9 version

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,19 +1,19 @@
 // NOTE: import only the Firebase modules that you need in your app... except
 // for the second line, which makes both the linter and react-firebase happy
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 // Initalize Firebase.
 var firebaseConfig = {
-  apiKey: "AIzaSyAxzJAKfHjf6z_VZ2bXyM872DmHRatY-Pw",
-  authDomain: "tcl-35-smart-shopping-list.firebaseapp.com",
-  projectId: "tcl-35-smart-shopping-list",
-  storageBucket: "tcl-35-smart-shopping-list.appspot.com",
-  messagingSenderId: "284148869267",
-  appId: "1:284148869267:web:29f0e9a532b46357ee0566"
+  apiKey: 'AIzaSyAxzJAKfHjf6z_VZ2bXyM872DmHRatY-Pw',
+  authDomain: 'tcl-35-smart-shopping-list.firebaseapp.com',
+  projectId: 'tcl-35-smart-shopping-list',
+  storageBucket: 'tcl-35-smart-shopping-list.appspot.com',
+  messagingSenderId: '284148869267',
+  appId: '1:284148869267:web:29f0e9a532b46357ee0566',
 };
 
-const firebaseInstance = firebase.initializeApp(firebaseConfig);
-const db = firebaseInstance.firestore();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export { db };
