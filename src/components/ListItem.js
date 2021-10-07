a6655474b86223f194054320ca9bd98b88fe0811
Fatima-ola <fatimaolasunkanmi01@gmail.com>
2021-10-07 02:39:05 +0100

    clean up read and write to firestore
    co-authored-by: Ayu Adiati <retno.ayu@gmail.com>
    co-authored-by: Fatima Olasunkanmi-Ojo <fatimaolasunkanmi01@gmail.com>

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { db } from '../lib/firebase.js';
+import { collection, getDocs } from 'firebase/firestore';
 
 const ListItem = () => {
+  const [items, setItems] = useState([]);
+  const itemsCollectionRef = collection(db, 'shopping-list');
+
+  // get items
+  useEffect(() => {
+    const getItems = async () => {
+      const data = await getDocs(itemsCollectionRef);
+      setItems(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getItems();
+  }, []);
+
   return (
     <div>
-      <h3>Items Listed</h3>
+      {items.map((item) => {
+        return (
+          <div>
+            <p>Name of Item: {item.itemName}</p>
+            <p>Buying Time: {item.buyingTime}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
