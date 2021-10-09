e8fa668aeba7217d1fbb8cf1b728d48ee1b8acc2
Fatima-ola <fatimaolasunkanmi01@gmail.com>
2021-10-09 17:41:58 +0100

    made changes

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -12,8 +12,8 @@ const AddItem = () => {
       setLoading(true);
 
       await addDoc(collection(db, 'shopping-list'), {
-        itemName: 'soap',
-        buyingTime: 'soon',
+        itemName: 'milk',
+        buyingTime: 'now',
       });
 
       setLoading(false);
