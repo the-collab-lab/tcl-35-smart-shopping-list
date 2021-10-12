9973e22d38e47467aad349ee9428d97094aae28d
Dana Chen <danaqchen@gmail.com>
2021-10-12 13:49:36 +0200

    updated week 2 user stories. co-authored by: Fatima <fatimaolasunkanmi01@gmail.com>

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { getToken, words } from '@the-collab-lab/shopping-list-utils';
 
 const Home = () => {
   const [token, setToken] = useState(getToken);
+  const history = useHistory();
 
   const handleClick = function () {
-    setToken(token);
+    let localToken = localStorage.getItem('currToken');
+    if (localToken) {
+      console.log('local token already exists');
+      history.push('/list');
+    } else {
+      setToken(token);
+    }
   };
 
   useEffect(() => {
@@ -15,15 +23,24 @@ const Home = () => {
 
   return (
     <header className="home-header">
-      <h1>Welcome to your Smart Shopping list!</h1>
-      <button onClick={() => handleClick()}>Create a new list</button>
-      <p>- or -</p>
-      <p>Join an existing shopping list by entering a three word token.</p>
-      <label htmlFor="token">
-        Share token
-        <input type="text" id="token"></input>
-      </label>
-      <button>Join an existing list</button>
+      <section className="welcome">
+        <h1>Welcome to your Smart Shopping list!</h1>
+      </section>
+      <section className="create">
+        <button onClick={() => handleClick()}>Create a new list</button>
+        <p>- or -</p>
+      </section>
+      <section className="join">
+        <p>Join an existing shopping list by entering a three word token.</p>
+        <form action="">
+          <div>
+            <label htmlFor="token">Share token</label>
+            <br />
+            <input type="text" id="token" />
+          </div>
+        </form>
+        <button>Join an existing list</button>
+      </section>
     </header>
   );
 };
