d2c3a20170523855dc442580aacedd27ab5b68dc
Dana Chen <danaqchen@gmail.com>
2021-10-18 11:06:47 +0200

    input validation

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { db } from '../lib/firebase';
 import { NavLink } from 'react-router-dom';
 import {
@@ -9,17 +9,48 @@ import {
   updateDoc,
   arrayUnion,
   setDoc,
+  onSnapshot,
 } from '@firebase/firestore';
 
 const AddItem = () => {
+  const [items, setItems] = useState([]);
   const [itemName, setItemName] = useState('');
   const [nextPurchase, setNextPurchase] = useState(0);
   const [lastPurchase, setLastPurchase] = useState(null);
+  const [emptyList, setEmptyList] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [showErrorMessage, setShowErrorMessage] = useState(true);
 
   const currToken = localStorage.getItem('currToken');
   const itemsCollectionRef = collection(db, 'shopping-list');
 
-  const validateInput = (list) => {};
+  useEffect(() => {
+    const getItems = async () => {
+      currToken &&
+        onSnapshot(doc(itemsCollectionRef, currToken), (doc) => {
+          if (!doc.data()) {
+            setEmptyList(true);
+          } else {
+            setItems(doc.data().items);
+          }
+        });
+    };
+    getItems();
+  }, []);
+
+  const cleanString = (str) => {
+    const regex = /[a-z]/g;
+    return str.trim().toLowerCase().match(regex).join('');
+  };
+
+  const validateInput = (list) => {
+    for (const item of items) {
+      if (cleanString(item.itemName) === cleanString(list.itemName)) {
+        return false;
+      }
+    }
+    return true;
+  };
 
   const addItems = async () => {
     const refData = await getDocs(itemsCollectionRef);
@@ -37,7 +68,7 @@ const AddItem = () => {
           items: arrayUnion(newList),
         });
       } else {
-        alert('item already exists in the list');
+        setErrorMessage('Item already exists in the list');
       }
     } else {
       await setDoc(doc(db, 'shopping-list', currToken), {
@@ -82,6 +113,8 @@ const AddItem = () => {
     <div id="main-container" className="add-items">
       <form id="sub-wrapper">
         <div className="form-item">
+          {errorMessage && <p className="error"> {errorMessage} </p>}
+
           <label htmlFor="itemName">What do you want to buy: </label>
           <input
             type="text"
