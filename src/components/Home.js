1a8b07957b05475b69df5e44a616356fb3ab9010
Fatima-ola <fatimaolasunkanmi01@gmail.com>
2021-10-18 16:43:01 +0100

    got the shared token value and stored in localstorage
    Co-authored-by: Edema edemaukabi@gmail.com

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { getToken, words } from '@the-collab-lab/shopping-list-utils';
 
 const Home = () => {
   // const existingToken = localStorage.getItem('currentToken');
+  const [joinlist, setJoinList] = useState('');
   const [token, setToken] = useState(() => {
     const existingToken = localStorage.getItem('currToken');
     return existingToken ? existingToken : '';
@@ -23,6 +24,22 @@ const Home = () => {
     history.push('/list');
   };
 
+  const handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+    if (name === 'joinlist') {
+      setJoinList(value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('clicked');
+    if (joinlist) {
+      localStorage.setItem('currToken', joinlist);
+      console.log(joinlist);
+    }
+  };
+
   return (
     <header className="home-header">
       <section className="welcome">
@@ -34,14 +51,20 @@ const Home = () => {
       </section>
       <section className="join">
         <p>Join an existing shopping list by entering a three word token.</p>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="token">Share token</label>
             <br />
-            <input type="text" id="token" />
+            <input
+              placeholder="three word token"
+              type="text"
+              name="joinlist"
+              value={joinlist}
+              onChange={handleChange}
+            />
           </div>
+          <button>Join an existing list</button>
         </form>
-        <button>Join an existing list</button>
       </section>
     </header>
   );
