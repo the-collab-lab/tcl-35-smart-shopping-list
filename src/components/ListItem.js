5a165a2835f9c5553a6fde97db3e64d863b66028
Dana Chen <danaqchen@gmail.com>
2021-10-25 20:36:14 +0200

    initial checkboxes

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,6 +3,7 @@ import { db } from '../lib/firebase.js';
 import { NavLink } from 'react-router-dom';
 import { collection, doc, onSnapshot } from 'firebase/firestore';
 import Footer from './Footer';
+import { check } from 'prettier';
 
 const ListItem = () => {
   const [items, setItems] = useState([]);
@@ -12,7 +13,11 @@ const ListItem = () => {
 
   const currToken = localStorage.getItem('currToken');
   const itemsCollectionRef = collection(db, 'shopping-list');
-
+  // console.log(items);
+  const [checkBoxes, setCheckBoxes] = useState(
+    new Array(items.length).fill(false),
+  );
+  // console.log(checkBoxes);
   // get items
   useEffect(() => {
     const getItems = async () => {
@@ -28,6 +33,23 @@ const ListItem = () => {
     getItems();
   }, []);
 
+  const handleOnChange = async (position) => {
+    for (let item of items) {
+      if (item['itemName'] === items[position]['itemName']) {
+        item['lastPurchase'] = new Date();
+      }
+    }
+
+    console.log(items);
+    await setCheckBoxes(new Array(items.length).fill(false));
+    const updatedCheckBoxes = checkBoxes.map((item, index) =>
+      index === position ? !item : item,
+    );
+
+    setCheckBoxes(updatedCheckBoxes);
+    setItems(items);
+  };
+
   return (
     <div id="main-container" className="flex-wrapper">
       <div id="sub-wrapper">
@@ -36,10 +58,22 @@ const ListItem = () => {
         {error && <p>An error occured</p>}
         {emptyList && <p>You dont have any list yet</p>}
 
-        {items.map((item) => {
+        {items.map((item, index) => {
           return (
-            <div className="item-wrapper">
-              <p>{item.itemName}</p>
+            <div key={index} className="item-wrapper">
+              <div className="left-section">
+                <input
+                  type="checkbox"
+                  id={`custom-checkbox-${index}`}
+                  // name={name}
+                  // value={name}
+                  checked={checkBoxes[index]}
+                  onChange={() => handleOnChange(index)}
+                />
+              </div>
+              <div className="right-section">
+                <p>{item.itemName}</p>
+              </div>
             </div>
           );
         })}
