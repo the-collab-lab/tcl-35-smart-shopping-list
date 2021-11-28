45727dbfb4c1022ec974df3b6ff363f37b236d6e
edemaukabi <edemaukabi@gmail.com>
2021-11-28 07:42:05 +0100

    style Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,20 +3,22 @@ import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <nav className="links-wrapper">
-      <ul className="links mt-5">
-        <li>
-          <NavLink to="/list" activeClassName="active">
-            List
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/add" activeClassName="active">
-            Add Item
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <footer className="footer">
+      <nav className="links-wrapper App-Navbar-Bottom fixed-bottom">
+        <ul className="links mt-5 nav nav-pills nav-fill">
+          <li className="nav-item">
+            <NavLink to="/list" activeClassName="active nav-link p-2 selected">
+              List
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/add" activeClassName="active nav-link p-2 selected">
+              Add Item
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </footer>
   );
 };
 
